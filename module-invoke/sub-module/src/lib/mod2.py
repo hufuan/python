09156bbbfbf0a6950d147a391ace63748678ddf2
fuhu <fuan.hu@netscout.com>
2021-11-04 23:34:02 +0800

    test

---
+++ module-invoke/sub-module/src/lib/mod2.py
@@ -0,0 +1,9 @@
+class Teacher():
+    def __init__(self, name, subject):
+        self.name = name
+        self.subject = subject
+
+    def detail(self):
+        print(self.name)
+        print(self.subject)
+    pass
---
+++ module-invoke/sub-module/src/test1.py
@@ -0,0 +1,8 @@
+from lib import mod2
+import mod1
+print("haha")
+teacher = mod2.Teacher("laochen", 50)
+student = mod1.Student("Lixi", 9)
+
+teacher.detail()
+student.detail()
